2d60a2cf3bb708969573f05981d20af02966720b
Reazul Islam <reazulislam137@gmail.com>
2018-05-04 00:00:32 +0600

    first commit

---
+++ may-04-01.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number = 0;
+
+    while (number != 10)
+    {
+        cout << number << endl;
+        number++;
+    }
+
+    cout << endl << "The loop has finishes " << endl;
+
+    return 0;
+}
